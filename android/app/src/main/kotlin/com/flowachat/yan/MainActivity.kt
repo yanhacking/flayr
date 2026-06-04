add9a0e50e0422f4df923507e65ce4c3dbecbb36
yanhacking <yanhaiti@outlook.fr>
2026-06-04 00:30:13 -0400

    new

--- android/app/src/main/kotlin/com/flowachat/yan/MainActivity.kt
+++ android/app/src/main/kotlin/com/flowachat/yan/MainActivity.kt
@@ -1,24 +1,5 @@
 package com.flowachat.yan
 
-import com.baseflow.permissionhandler.PermissionHandlerPlugin
-import com.retrytech.retrytech_plugin.RetrytechPlugin
-import com.revenuecat.purchases_flutter.PurchasesFlutterPlugin
-import com.tekartik.sqflite.SqflitePlugin
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
-import io.flutter.plugins.pathprovider.PathProviderPlugin
-import io.flutter.plugins.videoplayer.VideoPlayerPlugin
 
-class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        flutterEngine.plugins.add(GoogleMobileAdsPlugin())
-        flutterEngine.plugins.add(PathProviderPlugin())
-        flutterEngine.plugins.add(PurchasesFlutterPlugin())
-        flutterEngine.plugins.add(SqflitePlugin())
-        flutterEngine.plugins.add(VideoPlayerPlugin())
-        flutterEngine.plugins.add(PermissionHandlerPlugin())
-        flutterEngine.plugins.add(RetrytechPlugin())
-    }
-}
+class MainActivity : FlutterActivity()
